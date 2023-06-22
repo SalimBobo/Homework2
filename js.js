304b1b11e60c9b2f1a269930a1fba5fc184f97cb
SALIM TUKHTATOSHOV <63718957+SalimBobo@users.noreply.github.com>
2023-06-22 10:29:47 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+function toGolden() {
+    document.getElementsByTagName('span')[0].style='color: #FFE569';
+}
+function goMyWeb(){
+    document.getElementsByTagName('a')[0].href= 'https://salimbobo.github.io/scubalifebySalimBoBo/';
+    document.getElementsByTagName('a')[0].innerHTML='My Web';
+}
+
+function changer(){
+    document.getElementsByTagName('p')[2].style.display="none";
+    document.getElementsByTagName('p')[3].style.textAlign="center";
+    document.getElementsByTagName('p')[4].innerHTML=' 5. Hello World';
+}
+
+function myName(){
+    alert("Salim BoBo Tukhtatoshov");
+}
